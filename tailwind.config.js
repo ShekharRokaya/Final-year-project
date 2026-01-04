614fa2316fe45f327a7045efdefcb3de87fa7826
shikhar rokaya <shikharrokaya24@gmail.com>
2026-01-04 21:18:51 +0545

    skr

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors:{
+      'primary':"#5f6FFF"
+    },
+    gridTemplateColumns:{
+      'auto':'repeat(auto-fill, minmax(200px,1fr))'
+    },
+    },
+   
   },
   plugins: [],
 }
