2de0e9a69f2f7d53d840611012d7a7e51b380c07
shikhar rokaya <shikharrokaya24@gmail.com>
2026-01-05 17:00:46 +0545

    pop

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { ToastContainer } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import {StrictMode} from "react";
+import {createRoot} from "react-dom/client";
+import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+
+
+
+createRoot(document.getElementById("root")).render(
+ 
     <BrowserRouter>
-      <ToastContainer />
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    <AppContextProvider>
+       <App />
+    </AppContextProvider>
+   
+      
+    </BrowserRouter>,
+
 );
